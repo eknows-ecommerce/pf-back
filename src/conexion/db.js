34a72972c7276ab19d956445a801707b3bee0b45
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-02 11:27:11 -0500

    Relaciones
    Relaciones de cardinalidad de nuestros modelos

--- src/conexion/db.js
+++ src/conexion/db.js
@@ -1,14 +1,8 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-
-// API_KEY=206aac4c607943c49eb99bf20a699dad
-// API_URL=https://api.spoonacular.com/recipes/complexSearch
-// API_URL2=https://api.spoonacular.com/recipes/{id}/information
-// API_FLAG=&addRecipeInformation=true
-// API_URL100=https://api.spoonacular.com/recipes/complexSearch?apiKey=206aac4c607943c49eb99bf20a699dad&number=100
+require('dotenv').config()
+const { Sequelize, DataTypes } = require('sequelize')
+const fs = require('fs')
+const path = require('path')
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -16,38 +10,50 @@ const sequelize = new Sequelize(
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
-const basename = path.basename(__filename);
+)
+const basename = path.basename(__filename)
 
-const modelDefiners = [];
+const modelDefiners = []
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '../models'))
   .filter(
     (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
-  });
+    modelDefiners.push(require(path.join(__dirname, '../models', file)))
+  })
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize))
 // Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
+let entries = Object.entries(sequelize.models)
 let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
-]);
-sequelize.models = Object.fromEntries(capsEntries);
+])
+sequelize.models = Object.fromEntries(capsEntries)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-// const { Recipe, Diet, Step, User } = sequelize.models;
+const { Categoria, Libro, Media, Pedido, Tag, Usuario } = sequelize.models
 // Aca vendrian las relaciones
-// Recetas ==> N-M<== Dietas;
+// Categoria N<=>M Libro
+Categoria.belongsToMany(Libro, { through: 'CategoriaLibro' })
+Libro.belongsToMany(Categoria, { through: 'CategoriaLibro' })
+// Tag N<=>M Libro
+Tag.belongsToMany(Libro, { through: 'TagLibro' })
+Libro.belongsToMany(Tag, { through: 'TagLibro' })
+// Pedido N<=>M Libro
+const Detalle = sequelize.define('Detalle', { cantidad: DataTypes.INTEGER })
+Pedido.belongsToMany(Libro, { through: Detalle })
+Libro.belongsToMany(Pedido, { through: Detalle })
+// Libros 1=>N Media
+Libro.hasMany(Media)
+Media.belongsTo(Libro)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
-};
+}
