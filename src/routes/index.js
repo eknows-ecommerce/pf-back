f69c5a30cc897b922dbeb39a555ac2676dc816d2
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-02 16:59:43 -0500

    Routes - index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 //Importaciones de librerias y rutas
 const { Router } = require('express')
 const tag = require('./tag.js')
-
+const categoria = require('./categoria.js')
 //Configuracion de rutas
 const router = Router()
 
 // Ejemplo: router.use('/auth', authRouter);
-// router.use('/categoria', categoria)
+router.use('/categoria', categoria)
 // router.use('/libro', libro)
 router.use('/tag', tag)
 // router.use('/media', media)
