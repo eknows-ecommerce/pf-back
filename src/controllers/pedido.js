e71cfdb662e2ad656cdadb33bfbcdef90f3e84ae
kachamozo <kachamozo@yahoo.com>
2022-08-02 23:53:45 -0400

    deleteById Funcionando

--- src/controllers/pedido.js
+++ src/controllers/pedido.js
@@ -91,26 +91,26 @@ const updateById = async (req, res, next) => {
   }
 }
 
-// const deleteById = async (req, res, next) => {
-//   const { id } = req.params
-//   try {
-//     if (!id) return res.status(404).json({ msg: 'Id no provisto' })
-//     const tag = await Tag.findByPk(id)
-//     if (!tag) return res.status(404).json({ msg: 'Tag no encontrado' })
-//     const deleteTag = await tag.destroy()
-//     if (!deleteTag)
-//       return res.status(200).json({ msg: 'No se pudo eliminar el tag' })
-//     res.status(200).json({ tag, msg: 'Tag eliminado' })
-//   } catch (error) {
-//     next(error)
-//   }
-// }
+const deleteById = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    if (!id) return res.status(400).json({ msg: 'Id no provisto' })
+    const pedido = await Pedido.findByPk(id)
+    if (!pedido) return res.status(404).json({ msg: 'Pedido no encontrado' })
+    const deletePedido = await pedido.destroy()
+    if (!deletePedido)
+      return res.status(200).json({ msg: 'No se pudo eliminar el pedido' })
+    res.status(200).json({ pedido, msg: 'Pedido eliminado' })
+  } catch (error) {
+    next(error)
+  }
+}
 
 module.exports = {
   getAll,
   getById,
   create,
   updateById,
-  //   deleteById,
+  deleteById,
   // createBulk,
 }
