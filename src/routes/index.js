e45e540abdc25f1f551e3f9e393a81d20491ade7
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-08-02 22:47:19 -0300

    media
    creacionDeRutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@
 const { Router } = require('express')
 const tag = require('./tag.js')
 const categoria = require('./categoria.js')
+const media = require('./media.js')
 //Configuracion de rutas
 const router = Router()
 
@@ -9,7 +10,7 @@ const router = Router()
 router.use('/categoria', categoria)
 // router.use('/libro', libro)
 router.use('/tag', tag)
-// router.use('/media', media)
+router.use('/media', media)
 // router.use('/pedido', pedido)
 // router.use('/usuario', usuario)
 
