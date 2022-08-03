c74428efc9e4c3d29837b125e42853f45d65b789
nnicolasg <104043118+nnicolasg@users.noreply.github.com>
2022-08-03 17:50:59 -0300

    añadidas todas las rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@
 const { Router } = require('express')
 const tag = require('./tag.js')
 const categoria = require('./categoria.js')
+const libro = require('./libro.js')
+const pedido = require('./pedido')
+const usuario = require('./usuario')
+const media = require('./media')
 
 //Configuracion de rutas
 const router = Router()
@@ -10,6 +14,8 @@ const router = Router()
 router.use('/categoria', categoria)
 router.use('/libro', libro)
 router.use('/tag', tag)
-
+router.use('/pedido', pedido)
+router.use('/usuario', usuario)
+router.use('/media', media)
 
 module.exports = router
