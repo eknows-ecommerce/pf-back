d2c7454d6513f3c5b948da6f5029c10811e61798
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-02 21:18:36 -0500

    Rutas - libro - B
    Se agrego la ruta libro

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@
 const { Router } = require('express')
 const tag = require('./tag.js')
 const categoria = require('./categoria.js')
+const libro = require('./libro.js')
+
 //Configuracion de rutas
 const router = Router()
 
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/categoria', categoria)
-// router.use('/libro', libro)
+router.use('/libro', libro)
 router.use('/tag', tag)
 // router.use('/media', media)
 // router.use('/pedido', pedido)
