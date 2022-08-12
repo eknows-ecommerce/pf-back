bea576f6555bd7479fd9275da1cac3d520c5aff1
Jspena01 <josesamuelrd@gmail.com>
2022-08-12 08:21:38 -0300

    Ssl modificado

--- src/conexion/db.js
+++ src/conexion/db.js
@@ -10,8 +10,8 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    ssl: {
-      rejectUnauthorized: false,
+    dialectOptions: {
+      ssl: { require: true },
     },
   }
 )
