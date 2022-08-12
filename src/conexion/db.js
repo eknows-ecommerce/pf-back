205fb5803cba8b1b1d942f15b0b09db2d1eba186
Jspena01 <josesamuelrd@gmail.com>
2022-08-12 08:19:49 -0300

    Ssl modificado

--- src/conexion/db.js
+++ src/conexion/db.js
@@ -5,12 +5,14 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL } = process.env
 
 const sequelize = new Sequelize(
   DATABASE_URL
-    ? `${DATABASE_URL} sslmode=require`
+    ? `${DATABASE_URL}`
     : `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   }
 )
 const basename = path.basename(__filename)
